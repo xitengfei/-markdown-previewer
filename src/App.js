d16a470aa8fc6dc4d719eb1df1d97fea6055b55e
tengfei2015 <xitengfei@aragoncs.com>
2018-11-17 14:47:32 +0800

    try to add sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
+import marked from "marked"
 import logo from './logo.svg';
 import './App.css';
+import md from './data/md-example'
+import './MarkdownPreviewer.scss'
+
+const MarkdownInput = (props) => {
+  const handleChange = (evt) => {
+    props.onMarkdownChange(evt.target.value)
+  }
+  return (
+    <p>
+      <textarea
+        value={props.message}
+        onChange={handleChange}
+      >
+      </textarea>
+    </p>
+  )
+}
+
+class MarkdownView extends Component{
+  constructor(props){
+    super(props)
+
+    this.rawMarkup = this.rawMarkup.bind(this)
+  }
+
+  rawMarkup(message){
+    var rawMarkup = marked(message)
+    return {
+      __html: rawMarkup
+    }
+  }
+
+  render(){
+    return(
+      <div className="paper">
+        <div 
+          className="view" 
+          // onClick={this.handleClick}
+          dangerouslySetInnerHTML={this.rawMarkup(this.props.message)}
+          >
+        </div>
+      </div>
+    )
+  }
+}
+
+class MarkdownPreviewer extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      message:md
+    }
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <MarkdownInput message={this.state.message} />
+        <MarkdownView message={this.state.message} />
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        Markdown Previewer
         </header>
+        <main>
+          <MarkdownPreviewer />
+        </main>
+        <footer>
+          by tengfei.xi
+        </footer>
       </div>
     );
   }
---
+++ src/data/md-example.js
@@ -0,0 +1,35 @@
+var md = `Heading
+=======
+
+Sub-heading
+-----------
+ 
+### Another deeper heading
+ 
+> Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a  
+line break
+
+Text attributes *italic*, **bold**, \`code\`
+ ~~strikethrough~~ .
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in
+Spain.
+
+ *[Herman Fassett](https://freecodecamp.com/hermanfassett)*`;
+
+ export default md
